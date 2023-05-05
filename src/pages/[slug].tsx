0d5df794bab69fa090c0b53f03792ca76eec4c00
danlikestocode <79137382+danlikestocode@users.noreply.github.com>
2023-05-04 21:14:59 -0700

    feat: profile page with hydration

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,6 +1,7 @@
 import { type GetStaticProps, type NextPage } from "next";
 import Head from "next/head";
 import { api } from "~/utils/api";
+import Image from "next/image";
 
 const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
   const { data } = api.profile.getUserByUsername.useQuery({
@@ -8,15 +9,27 @@ const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
   });
 
   if (!data) return <div>404</div>;
+  if (!data.username) return <div>404</div>;
 
   return (
     <>
       <Head>
-        <title>{data.username}</title>
+        <title>{`${data.username}'s Profile`}</title>
       </Head>
-      <main className="flex h-screen justify-center">
-        <div>{data.username}</div>
-      </main>
+      <PageLayout>
+        <div className="relative h-36 bg-slate-600">
+          <Image
+            src={data.profileImageUrl}
+            height={148}
+            width={148}
+            alt="Profile Picture"
+            className="absolute bottom-0 left-0 -mb-[74px] ml-4 rounded-full border-4 border-black"
+          />
+        </div>
+        <div className="h-[74px]"></div>
+        <div className="p-4 text-2xl font-bold">{`@${data.username}`}</div>
+        <div className="w-full border-b border-slate-400"></div>
+      </PageLayout>
     </>
   );
 };
@@ -25,6 +38,7 @@ import { appRouter } from "~/server/api/root";
 import { prisma } from "~/server/db";
 import { createServerSideHelpers } from "@trpc/react-query/server";
 import superjson from "superjson";
+import { PageLayout } from "~/components/layout";
 
 export const getStaticProps: GetStaticProps = async (context) => {
   const ssg = createServerSideHelpers({
